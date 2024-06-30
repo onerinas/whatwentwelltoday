b02e3ce4e571e5cc5047b00f296c880ba9df0feb
Rinas <5433320+onerinas@users.noreply.github.com>
2024-06-30 15:04:43 +0400

    Show latest first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @post = Post.new
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
